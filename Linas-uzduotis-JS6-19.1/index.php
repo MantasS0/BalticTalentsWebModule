1004cf135cd5d577e664e2d02ce65813da326d3b
MantasS0 <man_tas_pats@yahoo.com>
2019-04-25 15:40:45 +0300

    2019.04.25 uzduotis 19.1 needs work.

---
+++ Linas-uzduotis-JS6-19.1/index.php
@@ -0,0 +1,15 @@
+<?php include 'header.php';  ?>
+
+
+
+
+
+<input id="input" placeholder="Enter a number..." onkeyup="reverse()">
+<input id="output" placeholder="Reversed number...">
+
+
+
+
+
+
+<?php include 'footer.php';?>
---
+++ Linas-uzduotis-JS6-19.1/js/script.js
@@ -0,0 +1,132 @@
+let reversedNumber ='';
+function reverse() {
+    let input = document.getElementById('input').value;
+    console.log(input);
+    if (isNaN(input)) {
+        alert('Not a number...');
+    } else {
+        input = Number.parseInt(input);
+        if (input < 0) {
+            reversedNumber += '-';
+            input *= -1;
+        }
+        console.log(input);
+        // while (input >= 1) {
+            reversedNumber += (input % 10).toString();
+            input%=10;
+            console.log(input);
+            console.log(reversedNumber);
+
+        // }
+        document.getElementById('output').value = reversedNumber;
+    }
+}
+
+
+/*
+/!*
+function perimetroSkaiciavimas(krastine, kitaKrastine, darKitaKrastine) {
+    var perimetras = krastine + kitaKrastine + darKitaKrastine;
+    console.log(perimetras);
+
+    return perimetras;
+}
+
+var a = 7;
+var b = 9;
+var c = 8;
+
+perimetroSkaiciavimas(a,b,c);
+
+var h = 9;
+var j = 10;
+var k = 11;
+
+
+var manoFunkcAtsakymas = perimetroSkaiciavimas(perimetroSkaiciavimas(h,j,k),j,k);
+
+console.log(manoFunkcAtsakymas);
+*!/
+
+function greitis(atstumasInKm, laikasInVal) {
+    let meters = atstumasInKm * 1000;
+    let sec = laikasInVal * 3600;
+    return meters / sec;
+}
+
+function printToScreen(speed) {
+    document.getElementById('ataskymas').innerHTML += `<div> Greitis yra: ${speed.toFixed(2)}m/sec </div>`;
+}
+
+printToScreen(greitis(100, 1));
+printToScreen(greitis(120, 2));
+printToScreen(greitis(200, 3.5));
+
+
+function addColor() {
+    document.body.style.backgroundColor = "red";
+}
+
+function removeColor() {
+    document.body.style.background = "none";
+}
+
+var isColor = false;
+var colorCode = "#66adf6";
+
+function toggleColor() {
+    if (isColor) {
+        document.body.style.background = "none";
+        isColor = false;
+    } else {
+
+        document.body.style.backgroundColor = colorCode;
+        isColor = true;
+    }
+
+}
+
+
+function changeColorCode(inputColor) {
+
+    let abecele = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'h', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'v', 'z'];
+
+    let colorString = inputColor;
+    let curentArray = colorString.toArray();
+    for (let i = 0; i < curentArray.length; i++) {
+        if (isNaN(curentArray[i])) {
+
+        }
+    }
+}
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
