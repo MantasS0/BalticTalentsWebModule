b748a2c36a88222dc0f69de6a4c9b35e1ddf4532
Mantas Sasnauskas <man_tas_pats@yahoo.com>
2019-04-25 12:28:52 +0300

    2019.04.25 po paskaitos.

--- Linas-JS6/index.php
+++ Linas-JS6/index.php
@@ -4,8 +4,11 @@
 
 
 
+<div id="ataskymas"></div>
 
-
+<button id="addColor" onclick="addColor()">Add Color</button>
+<button id="removeColor" onclick="removeColor()">Remove Color</button>
+<button id="toggleColor" onclick="toggleColor()">Toggle Color</button>
 
 
 
