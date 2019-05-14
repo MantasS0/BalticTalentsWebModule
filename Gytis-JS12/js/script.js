0d6a5c5ebcf44f602d276bf9d6120fa8d3e4fe23
MantasS0 <man_tas_pats@yahoo.com>
2019-05-14 19:40:01 +0300

    2019.05.14 Integrated sounds with HTML5 audio element and with JS audioContext. Need to work on setInterval.

--- Gytis-JS12/js/script.js
+++ Gytis-JS12/js/script.js
@@ -1,183 +1,84 @@
-const UI = {
-    loadingBar: document.querySelector('.loading .bar'),
-    wordElement: document.querySelector('.word'),
-    resultMessage: document.querySelector('h2'),
-    difficultyButtons: {
-        buttonEasy: document.querySelector('.difficulty .easy'),
-        buttonMedium: document.querySelector('.difficulty .medium'),
-        buttonHard: document.querySelector('.difficulty .hard')
+class Task {
+    constructor(text) {
+        this.text = text;
+        this.done = false;
     }
-};
-
-const gameData = {
-    currentWord: 'balionas',
-    progress: 0,
-    difficulty: 'easy',
-    possibleWords: ['balionas', 'stalas', 'projektorius', 'lsd', 'pica', 'kebabas', 'alus'],
-    looseResultMessage: ['Failure!', 'You are as smart as a potato', 'Congratulations, you are mentally challenged',
-        'A kindergartner is smarter then you', 'You are a disgrace', 'You are a waste of oxygen'],
-    winResultMessage: {
-        easy: ['Oh wow, you are as smart as a kindergartner', 'Congratulations, you are almost as smart as a chimpanzee',
-            'That\'s adorable'],
-        medium: ['I guess you studied in school', 'Your mother is a kind lady', 'Congratulations, you actually won'],
-        hard: ['Bullshit! You cheated!', 'Go buy a lottery ticket', 'Are you a Wizard?!', 'You lucky bastard']
-    }
-
-};
-
-
-class Game {
-
-    // var constantGameStateChecker = setInterval(()=>{this.checkGameState();},200);
-
-    constructor() {
-        this.generateNewWord();
-        UI.difficultyButtons.buttonEasy.addEventListener('click', this.setDifficulty.bind(this));
-        UI.difficultyButtons.buttonMedium.addEventListener('click', this.setDifficulty.bind(this));
-        UI.difficultyButtons.buttonHard.addEventListener('click', this.setDifficulty.bind(this));
-
-        document.addEventListener('keypress', this.checkKey.bind(this));
-
-        /*
-                setInterval(() => {
-                    // this.addProgress(0.3);
-                    this.checkGameState();
-                }, 100)
-        */
-
-    }
-
-
-    setDifficulty(e) {
-        let pressedButton = e.target.className;
-
-        if (pressedButton.includes('easy')) {
-            gameData.difficulty = 'easy';
-
-            UI.difficultyButtons.buttonEasy.classList.remove('inactive');
-            UI.difficultyButtons.buttonEasy.classList.add('active');
-
-            UI.difficultyButtons.buttonMedium.classList.remove('active');
-            UI.difficultyButtons.buttonMedium.classList.add('inactive');
-
-            UI.difficultyButtons.buttonHard.classList.remove('active');
-            UI.difficultyButtons.buttonHard.classList.add('inactive');
-
-        } else if (pressedButton.includes('medium')) {
-            gameData.difficulty = 'medium';
-
-            UI.difficultyButtons.buttonEasy.classList.remove('active');
-            UI.difficultyButtons.buttonEasy.classList.add('inactive');
-
-            UI.difficultyButtons.buttonMedium.classList.remove('inactive');
-            UI.difficultyButtons.buttonMedium.classList.add('active');
-
-            UI.difficultyButtons.buttonHard.classList.remove('active');
-            UI.difficultyButtons.buttonHard.classList.add('inactive');
-
-        } else if (pressedButton.includes('hard')) {
-            gameData.difficulty = 'hard';
-
-            UI.difficultyButtons.buttonEasy.classList.remove('active');
-            UI.difficultyButtons.buttonEasy.classList.add('inactive');
-
-            UI.difficultyButtons.buttonMedium.classList.remove('active');
-            UI.difficultyButtons.buttonMedium.classList.add('inactive');
-
-            UI.difficultyButtons.buttonHard.classList.remove('inactive');
-            UI.difficultyButtons.buttonHard.classList.add('active');
-        }
-
-    }
-
-    generateLetters() {
-        UI.wordElement.innerHTML = "";
-        for (let i = 0; i < gameData.currentWord.length; i++) {
-            UI.wordElement.innerHTML += `<div class="letter"></div>`;
-        }
-    }
-
-    checkKey(e) {
-        let letterFound = false;
-        for (let i = 0; i < gameData.currentWord.length; i++) {
-            let letter = gameData.currentWord[i];
-
-            if (letter === e.key) {
-                UI.wordElement.children[i].innerHTML = letter;
-                letterFound = true;
-            }
-        }
-        if (!letterFound) {
-            this.addProgress(10);
-        }
-        this.checkGameState();
-    }
-
-    checkGameState() {
-        if (this.gameLost()) {
-            clearInterval();
-            const randomIndex = Math.floor(Math.random() * gameData.looseResultMessage.length);
-            UI.resultMessage.innerHTML = gameData.looseResultMessage[randomIndex];
-            setTimeout(() => {
-                this.generateNewWord();
-                clearTimeout();
-            }, 3000);
-        }else if (this.gameWon()) {
-            clearInterval();
-            if (gameData.difficulty === 'easy') {
-                const randomIndex = Math.floor(Math.random() * gameData.winResultMessage.easy.length);
-                UI.resultMessage.innerHTML = gameData.winResultMessage.easy[randomIndex];
-            } else if (gameData.difficulty === 'medium') {
-                const randomIndex = Math.floor(Math.random() * gameData.winResultMessage.medium.length);
-                UI.resultMessage.innerHTML = gameData.winResultMessage.medium[randomIndex];
-            } else if (gameData.difficulty === 'hard') {
-                const randomIndex = Math.floor(Math.random() * gameData.winResultMessage.hard.length);
-                UI.resultMessage.innerHTML = gameData.winResultMessage.hard[randomIndex];
-            }
-            setTimeout(() => {
-                this.generateNewWord();
-                clearTimeout();
-            }, 3000);
-
-        }
-    }
-
-    addProgress(amount) {
-        gameData.progress = Math.min(100, gameData.progress + amount);
-        this.drawProgress();
-    }
-
-    drawProgress() {
-        UI.loadingBar.style.width = `${gameData.progress}%`;
-    }
-
-    gameLost() {
-        return gameData.progress === 100;
-    }
-
-    gameWon() {
-        for (let letter of UI.wordElement.children) {
-            if (letter.innerHTML === '') {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    generateNewWord() {
-        gameData.progress = 0;
-        this.drawProgress();
-        const randomWordIndex = Math.floor(Math.random() * gameData.possibleWords.length);
-        gameData.currentWord = gameData.possibleWords[randomWordIndex];
-        this.generateLetters();
-        UI.resultMessage.innerHTML = "";
-    }
-
 
 }
 
+const listView = document.querySelector('ul');
+const todoInput = document.querySelector('input');
+const addButton = document.querySelector('.button');
+const inputElement = document.querySelectorAll('ul li');
 
-new Game();
+const tasks = [];
+
+addButton.addEventListener('click', () => {
+    tasks.push(new Task(todoInput.value));
+
+    drawTasks();
+});
+
+function drawTasks() {
+    listView.innerHTML = '';
+    tasks.forEach((task, index) => {
+        let className = task.done ? 'done' : '';
+
+        let imageToShow = task.done ? 'img/share.png' : 'img/dribble.png';
+
+        listView.innerHTML += `<li class="${className}" onclick="taskDone(${index})">${task.text}</li>`;
+    });
+}
+
+function taskDone(taskIndex) {
+    if (tasks[taskIndex].done) {
+        tasks.splice(taskIndex, 1);
+    } else {
+        tasks[taskIndex].done = true;
+    }
+    drawTasks();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- Gytis-JS14/js/script.js
+++ Gytis-JS14/js/script.js
@@ -1,84 +1,220 @@
-class Task {
-    constructor(text) {
-        this.text = text;
-        this.done = false;
+const UI = {
+    loadingBar: document.querySelector('.loading .bar'),
+    wordElement: document.querySelector('.word'),
+    resultMessage: document.querySelector('h2'),
+    gameAudio: document.getElementById('gameAudio'),
+    difficultyButtons: {
+        buttonEasy: document.querySelector('.difficulty .easy'),
+        buttonMedium: document.querySelector('.difficulty .medium'),
+        buttonHard: document.querySelector('.difficulty .hard')
+    }
+};
+
+const gameData = {
+    currentWord: 'balionas',
+    progress: 0,
+    difficulty: 'easy',
+    possibleWords: ['balionas', 'stalas', 'projektorius', 'lsd', 'pica', 'kebabas', 'alus'],
+    looseResultMessage: ['Failure!', 'You are as smart as a potato', 'Congratulations, you are mentally challenged',
+        'A kindergartner is smarter then you', 'You are a disgrace', 'You are a waste of oxygen'],
+    winResultMessage: {
+        easy: ['Oh wow, you are as smart as a kindergartner', 'Congratulations, you are almost as smart as a chimpanzee',
+            'That\'s adorable'],
+        medium: ['I guess you studied in school', 'Your mother is a kind lady', 'Congratulations, you actually won'],
+        hard: ['Bullshit! You cheated!', 'Go buy a lottery ticket', 'Are you a Wizard?!', 'You lucky bastard']
+    }
+};
+
+const gameSounds = {
+    audioContext: a = new AudioContext(),
+    beep: function beep(vol, freq, duration) {
+        let v = a.createOscillator();
+        let u = a.createGain();
+        v.connect(u);
+        v.frequency.value = freq;
+        v.type = "sine";
+        u.connect(a.destination);
+        u.gain.value = vol * 0.01;
+        v.start(a.currentTime);
+        v.stop(a.currentTime + duration * 0.001);
+    },
+    boop: function beep(vol, freq, duration) {
+        let v = a.createOscillator();
+        let u = a.createGain();
+        v.connect(u);
+        v.frequency.value = freq;
+        v.type = "triangle";
+        u.connect(a.destination);
+        u.gain.value = vol * 0.01;
+        v.start(a.currentTime);
+        v.stop(a.currentTime + duration * 0.001);
+    }
+};
+
+
+class Game {
+
+    // var constantGameStateChecker = setInterval(()=>{this.checkGameState();},200);
+
+    constructor() {
+        this.generateNewWord();
+        UI.difficultyButtons.buttonEasy.addEventListener('click', this.setDifficulty.bind(this));
+        UI.difficultyButtons.buttonMedium.addEventListener('click', this.setDifficulty.bind(this));
+        UI.difficultyButtons.buttonHard.addEventListener('click', this.setDifficulty.bind(this));
+
+        document.addEventListener('keypress', this.checkKey.bind(this));
+
+        /*
+                setInterval(() => {
+                    // this.addProgress(0.3);
+                    this.checkGameState();
+                }, 100)
+        */
+
     }
 
-}
 
-const listView = document.querySelector('ul');
-const todoInput = document.querySelector('input');
-const addButton = document.querySelector('.button');
-const inputElement = document.querySelectorAll('ul li');
+    setDifficulty(e) {
+        gameSounds.beep(10, 261.63, 100);
+        let pressedButton = e.target.className;
 
-const tasks = [];
+        if (pressedButton.includes('easy')) {
+            gameData.difficulty = 'easy';
 
-addButton.addEventListener('click', () => {
-    tasks.push(new Task(todoInput.value));
+            UI.difficultyButtons.buttonEasy.classList.remove('inactive');
+            UI.difficultyButtons.buttonEasy.classList.add('active');
 
-    drawTasks();
-});
+            UI.difficultyButtons.buttonMedium.classList.remove('active');
+            UI.difficultyButtons.buttonMedium.classList.add('inactive');
 
-function drawTasks() {
-    listView.innerHTML = '';
-    tasks.forEach((task, index) => {
-        let className = task.done ? 'done' : '';
+            UI.difficultyButtons.buttonHard.classList.remove('active');
+            UI.difficultyButtons.buttonHard.classList.add('inactive');
 
-        let imageToShow = task.done ? 'img/share.png' : 'img/dribble.png';
+        } else if (pressedButton.includes('medium')) {
+            gameData.difficulty = 'medium';
 
-        listView.innerHTML += `<li class="${className}" onclick="taskDone(${index})">${task.text}</li>`;
-    });
-}
+            UI.difficultyButtons.buttonEasy.classList.remove('active');
+            UI.difficultyButtons.buttonEasy.classList.add('inactive');
 
-function taskDone(taskIndex) {
-    if (tasks[taskIndex].done) {
-        tasks.splice(taskIndex, 1);
-    } else {
-        tasks[taskIndex].done = true;
+            UI.difficultyButtons.buttonMedium.classList.remove('inactive');
+            UI.difficultyButtons.buttonMedium.classList.add('active');
+
+            UI.difficultyButtons.buttonHard.classList.remove('active');
+            UI.difficultyButtons.buttonHard.classList.add('inactive');
+
+        } else if (pressedButton.includes('hard')) {
+            gameData.difficulty = 'hard';
+
+            UI.difficultyButtons.buttonEasy.classList.remove('active');
+            UI.difficultyButtons.buttonEasy.classList.add('inactive');
+
+            UI.difficultyButtons.buttonMedium.classList.remove('active');
+            UI.difficultyButtons.buttonMedium.classList.add('inactive');
+
+            UI.difficultyButtons.buttonHard.classList.remove('inactive');
+            UI.difficultyButtons.buttonHard.classList.add('active');
+        }
+        this.generateNewWord();
+
     }
-    drawTasks();
+
+    generateLetters() {
+        UI.wordElement.innerHTML = "";
+        for (let i = 0; i < gameData.currentWord.length; i++) {
+            UI.wordElement.innerHTML += `<div class="letter"></div>`;
+        }
+    }
+
+    checkKey(e) {
+        gameSounds.boop(10, 87.31, 150);
+        let letterFound = false;
+        for (let i = 0; i < gameData.currentWord.length; i++) {
+            let letter = gameData.currentWord[i];
+
+            if (letter === e.key) {
+                UI.wordElement.children[i].innerHTML = letter;
+                letterFound = true;
+            }
+        }
+
+        !letterFound && gameData.difficulty === 'easy' ? this.addProgress(10) : false;
+        !letterFound && gameData.difficulty === 'medium' ? this.addProgress(20) : false;
+        !letterFound && gameData.difficulty === 'hard' ? this.addProgress(50) : false;
+        this.checkGameState();
+    }
+
+    checkGameState() {
+        if (this.gameLost()) {
+            clearInterval();
+            UI.gameAudio.src = 'sounds/lose.mp3';
+            UI.gameAudio.load();
+            UI.gameAudio.play();
+            const randomIndex = Math.floor(Math.random() * gameData.looseResultMessage.length);
+            UI.resultMessage.innerHTML = gameData.looseResultMessage[randomIndex];
+            setTimeout(() => {
+                this.generateNewWord();
+                UI.gameAudio.pause();
+                clearTimeout();
+            }, 4000);
+        } else if (this.gameWon()) {
+            clearInterval();
+            UI.gameAudio.src = 'sounds/win.mp3';
+            UI.gameAudio.load();
+            UI.gameAudio.play();
+            if (gameData.difficulty === 'easy') {
+                const randomIndex = Math.floor(Math.random() * gameData.winResultMessage.easy.length);
+                UI.resultMessage.innerHTML = gameData.winResultMessage.easy[randomIndex];
+            } else if (gameData.difficulty === 'medium') {
+                const randomIndex = Math.floor(Math.random() * gameData.winResultMessage.medium.length);
+                UI.resultMessage.innerHTML = gameData.winResultMessage.medium[randomIndex];
+            } else if (gameData.difficulty === 'hard') {
+                const randomIndex = Math.floor(Math.random() * gameData.winResultMessage.hard.length);
+                UI.resultMessage.innerHTML = gameData.winResultMessage.hard[randomIndex];
+            }
+            setTimeout(() => {
+                this.generateNewWord();
+                clearTimeout();
+            }, 4000);
+
+        }
+    }
+
+    addProgress(amount) {
+        gameData.progress = Math.min(100, gameData.progress + amount);
+        this.drawProgress();
+    }
+
+    drawProgress() {
+        UI.loadingBar.style.width = `${gameData.progress}%`;
+    }
+
+    gameLost() {
+        return gameData.progress === 100;
+    }
+
+    gameWon() {
+        for (let letter of UI.wordElement.children) {
+            if (letter.innerHTML === '') {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    generateNewWord() {
+        gameData.progress = 0;
+        this.drawProgress();
+        const randomWordIndex = Math.floor(Math.random() * gameData.possibleWords.length);
+        gameData.currentWord = gameData.possibleWords[randomWordIndex];
+        this.generateLetters();
+        UI.resultMessage.innerHTML = "";
+    }
+
+
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+new Game();
 
 
 
